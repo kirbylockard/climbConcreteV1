6630f8003238a55be3eae4ddf3a7857d072ad0bb
Kirby Lockard <kirbylockard@gmail.com>
2022-03-13 14:39:08 -0600

    fixed overscroll for anchors

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ const toggleTop = function (element) {
 
 toggleTop(navMenu);
 
+// FIX OVERSCROLL ON ANCHORS
+let about = document.querySelector("#about");
+let contact = document.querySelector("#contact");
+let anchors = [about, contact];
+
+for ( let i = 0; i < anchors.length; i++) {
+  anchors[i].style.paddingTop = `${headerheightnum.toString() + "px"}`;
+  anchors[i].style.marginTop = `${(headerheightnum * -1).toString() + "px"}`;
+  console.log(anchors[i].style);
+}
+
 //HAMBURGER OPEN AND CLOSE
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
