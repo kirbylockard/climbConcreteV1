12c07daea642e82dce3f24baf9d28b8c61742826
Kirby Lockard <kirbylockard@gmail.com>
2022-03-15 18:10:09 -0600

    smooth scroll did not work. Used intersection observer for transitions on scroll for cards and header. Position of nav-menu neeeds to be reworked. Header transitions need to be addressed for tablet and desktop

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const navMenu = document.querySelector(".nav-menu");
 const navLinks = document.querySelectorAll(".nav-link");
 const header = document.getElementById("header");
 const toTop = document.querySelector(".uparr");
-
+const navBar = document.querySelector(".nav-bar");
+const navLogo = document.querySelector(".nav-logo");
+const landing = document.querySelector(".landing");
+let navMenuPadding = parseInt(window.getComputedStyle(navMenu).paddingBottom);
 
 // let headerheightnum = document.getElementById("header").clientHeight;
-let headerheightnum = parseInt(window.getComputedStyle(header).height);
-console.log(headerheightnum);
+let headerheightnum = parseInt(window.getComputedStyle(header).height); 
+headerheightnum = headerheightnum + navMenuPadding;
+console.log(`HEADER ${headerheightnum}`);
+console.log(`NAVPADDING ${navMenuPadding}`);
 
 //dynamically position nav-menu based on current header height
 const toggleTop = function (element) {
   console.log(hamburger.style.display);
   if (window.getComputedStyle(hamburger).display == "none" ) {
-    console.log("navMenu style here");
-    console.log(element.style);
     return;
   }
   else if (!element.classList.contains("open")) {
@@ -43,12 +46,12 @@ for ( let i = 0; i < anchors.length; i++) {
 }
 
 //create smooth scroll function
-const smoothScroll = function(link) {
-  let href = link.getAttribute('href');
-  console.log(href);
-  let target = document.querySelector(`${href}`);
-  target.scrollIntoView({behavior: "smooth"});
-}
+// const smoothScroll = function(link) {
+//   let href = link.getAttribute('href');
+//   console.log(href);
+//   let target = document.querySelector(`${href}`);
+//   target.scrollIntoView({behavior: "smooth"});
+// }
 
 //HAMBURGER OPEN AND CLOSE
 hamburger.addEventListener("click", () => {
@@ -59,10 +62,53 @@ hamburger.addEventListener("click", () => {
 navLinks.forEach (link => link.addEventListener("click", () => {
   hamburger.classList.remove("open");
   toggleTop(navMenu);
-  smoothScroll(link);
+  //smoothScroll(link);
 }))
 
 
 
-//INTERSECTION OBSERVER for HEADER CHANGES
+//INTERSECTION OBSERVER for HEAD CHANGES
+const headerOptions = {
+  rootMargin: `-${headerheightnum}px 0px 0px 0px`
+};
 
+const headObserver = new IntersectionObserver(function(
+  entries,
+  headObserver
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        navBar.classList.add("nav-scrolled");
+        navLogo.classList.add("nav-scrolled");
+      } else if (entry.isIntersecting) {
+        navBar.classList.remove("nav-scrolled");
+        navLogo.classList.remove("nav-scrolled");
+      } 
+    })
+  }, 
+  headerOptions);
+
+  headObserver.observe(landing);
+
+
+//INTERSECTION OBSERVER for CARD CHANGES
+
+const callback = (entries) => {
+  entries.forEach(
+    (entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("card-view")
+      } else {
+        entry.target.classList.remove("card-view")
+      }
+    }
+  )
+}
+
+let cardObserver = new IntersectionObserver(callback);
+
+const animationItems = document.querySelectorAll(".pillar-card");
+
+animationItems.forEach(item => {
+  cardObserver.observe(item);
+})
